009d0c2cf19eb47d75ff8c94e93baecf9044e6db
Dvir Gross <dviross@outlook.com>
2022-10-20 19:44:25 +0300

    Dockerfile with entrypoint.sh working, serving web lavanga, also init.sh to down and up

---
+++ project/init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo -e "\n\nremoving old containers, images:\n\n"
+docker rm -f $(docker ps -aq)
+docker image rm -f $(docker images -q)
+
+echo -e "\n\n now building and runnig if build is well--\n\n"
+docker build --no-cache -t dvir-lavagna . && docker run -it -p 8080:8080 dvir-lavagna || echo -e "\n docker run went wrong!!\nexit" 
+
