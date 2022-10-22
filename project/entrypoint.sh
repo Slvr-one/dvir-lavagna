4309b6c77f0ca83f6dc36e0b8438b284f99c767c
Dvir Gross <dviross@outlook.com>
2022-10-23 02:39:13 +0300

    probablt a working version of this using the HSQLDB

--- project/entrypoint.sh
+++ project/entrypoint.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
-#mvn jetty:run
+java -Xms64m -Xmx128m -Ddatasource.dialect="${DB_DIALECT}" \
+    -Ddatasource.url="${DB_URL}" \
+    -Ddatasource.username="${DB_USER}" \
+    -Ddatasource.password="${DB_PASS}" \
+    -Dspring.profiles.active="${SPRING_PROFILE}" \
+    -jar lavagna-jetty-console.war --headless
 
 
 #unzip SNAPSHOT and execute outputed script:
-unzip ./target/lavagna-1.1.3-SNAPSHOT-distribution.zip
-./lavagna-1.1.3-SNAPSHOT/bin/lavagna.sh
-
-
-
-# Execute the web archive
-#CMD java -Xms64m -Xmx128m -Ddatasource.dialect="${DB_DIALECT}" \
-#-Ddatasource.url="${DB_URL}" \
-#-Ddatasource.username="${DB_USER}" \
-#Ddatasource.password="${DB_PASS}" \
-#-Dspring.profiles.active="${SPRING_PROFILE}" \
-#-jar ./target/lavagna-jetty-console.war --headless
+#unzip ./target/lavagna-1.1.3-SNAPSHOT-distribution.zip
+#./lavagna-1.1.3-SNAPSHOT/bin/lavagna.sh
--- project/init.sh
+++ project/init.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-echo -e "\n\nremoving old containers, images:\n\n"
+echo -e "\n\nremoving old containers\n\n"
 docker rm -f $(docker ps -aq)
-docker image rm -f $(docker images -q)
+#docker image rm -f $(docker images -q)
 
 echo -e "\n\n now building and runnig if build is well--\n\n"
-docker build --no-cache -t dvir-lavagna . && docker run -it -p 8080:8080 dvir-lavagna || echo -e "\n docker run went wrong!!\nexit" 
+#docker build --no-cache -t dvir-lavagna . && docker run -it -p 8080:8080 dvir-lavagna || echo -e "\n docker run went wrong!!\nexit"
+docker build -t lavagna . && docker run -itd -p 8080:8080 --name dvir-lavagna lavagna || echo -e "\n docker run went wrong!!\nexit" 
 
